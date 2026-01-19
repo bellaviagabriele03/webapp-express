28e1df2e2cb0ce42e7fb6d129a9e1f9083b8a980
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-19 17:53:37 +0100

    feat: function index DONE

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,17 @@
 import connection from "../database/db-connection.js";
 
 function index(req, res, next) {
+    const query = `SELECT * FROM movies`
+    connection.query(query, (err, result) => {
+        if (err) return next(err)
 
+        const movies = result;
+        res.status(200);
+        res.json({
+            count: movies.length,
+            result: movies
+        })
+    })
 }
 
 function show(req, res, next) {
