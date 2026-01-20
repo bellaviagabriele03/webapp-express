1cad6c8f0c51b8e7392e4cbb65a21cb35a8e10c5
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-20 17:13:50 +0100

    Pending changes before cleaning git history

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ import express from "express"
 import router from "./routers/movie.js";
 import routeNotFound from "./middlewares/routeNotFound.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import cors from "cors"
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
-app.use("/api/movies", router)
+
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
+
 app.use(express.json())
+app.use(express.static("public"))
 
-app.use(routeNotFound)
+
+
+app.use("/api/movies", router)
+
+
+
 app.use(errorHandler)
+app.use(routeNotFound)
 
-app.listen(port, () => {
-    console.log(`Server in ascolto sulla porta: ${port}`);
+
+app.listen(port, (err) => {
+    if (err) throw err;
     
+    console.log(`Server in ascolto sulla porta: ${port}`);
+
 })
